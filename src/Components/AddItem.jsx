bb05a1621b904e0caefa2b2daea520b55d64357c
EhoodGoren <ehoodgoren@gmail.com>
2021-12-25 23:25:16 +0200

    Added current quantity changing. Missing quantity is updated accordingly

--- src/Components/AddItem.jsx
+++ src/Components/AddItem.jsx
@@ -5,11 +5,11 @@ function AddItem() {
         <form>
             <h2>Add Item</h2>
             <label htmlFor='name' >Item name</label><br />
-            <input name='name' placeholder="Enter name" />
+            <input name='name' placeholder="Enter item name" />
             <label htmlFor='full'>Full quantity</label><br />
-            <input name='full' placeholder="Enter work place" />
+            <input name='full' placeholder="Enter full quantity" />
             <label htmlFor='current'>Current quantity</label><br />
-            <input name='current' type='number' />
+            <input name='current' type='number' placeholder={0} />
             <button type='submit'>Submit</button>
         </form>
     )
--- src/Components/ItemRow.jsx
+++ src/Components/ItemRow.jsx
@@ -1,14 +1,38 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { setItem } from '../redux/actions/equipmentActions';
 
-function ItemRow({ index, name, fullQuantity, currentQuantity }) {
+function ItemRow({ index, name, fullQuantity, currentQuantity, setItem }) {
+    const setQuantity = (e) => {
+        const inputValue = e.target.value;
+        let permitedValue = inputValue;
+        if(inputValue > fullQuantity) permitedValue = fullQuantity;
+        if(inputValue < 0) permitedValue = 0;
+        setItem(name, permitedValue);
+    }
     return (
         <tr>
             <td>{index}</td>
             <td>{name}</td>
             <td>{fullQuantity}</td>
-            <td>{currentQuantity}</td>
+            <td><input
+                    className='current-quantity'
+                    type='number'
+                    placeholder={0}
+                    min={0}
+                    max={fullQuantity}
+                    value={currentQuantity}
+                    onChange={setQuantity}
+                /></td>
+            <td>{fullQuantity - currentQuantity}</td>
         </tr>
     )
 }
 
-export default ItemRow;
+function mapDispatchToProps(dispatch) {
+    return {
+        setItem: (item, quantity) => dispatch(setItem(item, quantity))
+    }
+}
+
+export default connect(undefined, mapDispatchToProps)(ItemRow);

ee51971cdb4e7f0847c87f1523d8c95d34bdc820
EhoodGoren <ehoodgoren@gmail.com>
2021-12-25 18:06:32 +0200

    Set up login and equipment paths and elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './Components/Login';
+import Equipment from './Components/Equipment';
 
 function App() {
     return (
-        <div></div>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/login' element={<Login />} />
+                <Route path='/equipment' element={<Equipment />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
---
+++ src/Components/Equipment.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Equipment() {
+    return(
+        <div></div>
+    )
+}
+
+export default Equipment;

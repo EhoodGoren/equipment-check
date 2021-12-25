8f810cbed91edcca6f2d772b65531f91f0dda0d2
EhoodGoren <ehoodgoren@gmail.com>
2021-12-26 01:26:04 +0200

    Styled login form

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from "react";
 import { connect } from 'react-redux';
 import { useNavigate } from "react-router-dom";
 import { setUser } from "../redux/actions/userActions";
+import './Login.css';
 
 function Login({ setUser }) {
     const nameInput = useRef();
@@ -14,15 +15,21 @@ function Login({ setUser }) {
         navigate('/equipment');
     }
     return(
-        <div id='login'>
-            <h2>Worker details</h2>
-            <label htmlFor='name' >Full name</label><br />
-            <input name='name' placeholder="Enter name" ref={nameInput} />
-            <label htmlFor='workplace'>Workplace</label><br />
-            <input name='workplace' placeholder="Enter work place" ref={workplaceInput} />
-            <label htmlFor='date'>Date</label><br />
-            <input name='date' type='date' ref={dateInput} />
-            <button onClick={submitUser}>Submit</button>
+        <div id='login-form'>
+            <h2 id='login-title'>Worker details</h2>
+            <div id='login-name'>
+                <label htmlFor='name' >Full name</label><br />
+                <input name='name' placeholder="Enter name" ref={nameInput} />
+            </div>
+            <div id='login-workplace'>
+                <label htmlFor='workplace'>Workplace</label><br />
+                <input name='workplace' placeholder="Enter work place" ref={workplaceInput} />
+            </div>
+            <div id='login-date'>
+                <label htmlFor='date'>Date</label><br />
+                <input name='date' type='date' ref={dateInput} />
+            </div>
+            <button id='login-submit' onClick={submitUser}>Submit</button>
         </div>
     )
 }
